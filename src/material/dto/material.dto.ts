1258007862d877a39d248a8df393ce2812564170
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-14 21:20:27 +0700

    fix: schema

--- src/material/dto/material.dto.ts
+++ src/material/dto/material.dto.ts
@@ -2,6 +2,7 @@ import { FILE_TYPE } from "@prisma/client";
 
 export class CreateMaterialDto {
     courseSectionId: string;
+    transcripted: string;
     name: string;
     description: string;
     fileUrl: string;
@@ -10,6 +11,7 @@ export class CreateMaterialDto {
 
 export class UpdateMaterialDto {
     name?: string;
+    transcripted?: string;
     description?: string;
     fileUrl?: string;
     fileType?: FILE_TYPE;
--- src/material/material.service.ts
+++ src/material/material.service.ts
@@ -7,6 +7,36 @@ export class MaterialService {
     constructor(private readonly prisma: PrismaService) { }
 
     async createMaterial(data: CreateMaterialDto) { 
+        const context = 'You are a helpful assistant.Please elaborate and summarize the following text in Bahasa Indonesia, jawab dalam plain text dan tidak ada markdown sama sekali'
+
+        const chatResponse = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+            },
+            body: JSON.stringify({
+                model: "gpt-4o",
+                messages: [
+                    { role: "system", content: "You are a helpful assistant." },
+                    ...(context ? [{ role: "assistant", content: context }] : []),
+                    { role: "user", content: data.transcripted },
+                ],
+                max_tokens: 10000,
+            }),
+        });
+
+        console.log("Chat API response:", chatResponse);
+
+        if (!chatResponse.ok) {
+            const errorData = await chatResponse.json();
+            console.error("Chat API error:", errorData);
+            throw new Error("Failed to generate response");
+        }
+
+        const chatData = await chatResponse.json(); 
+        const responseText = chatData.choices[0].message.content;
+
         return this.prisma.material.create({
             data: {
                 courseItem: {
@@ -17,6 +47,7 @@ export class MaterialService {
                         type: 'MATERIAL',
                     }
                 },
+                transcripted: responseText  ,
                 fileUrl: data.fileUrl,
                 fileType: data.fileType,
             },
